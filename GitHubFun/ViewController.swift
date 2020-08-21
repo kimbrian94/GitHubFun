e1b10a7f237a31e16efdf1c42b219d1d3c0aaaf2
Brian Kim <kim.brian94@gmail.com>
2020-08-20 20:22:25 -0400

    Initial Commit

---
+++ GitHubFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFun
+//
+//  Created by Brian Kim on 2020-08-20.
+//  Copyright © 2020 Brian Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
